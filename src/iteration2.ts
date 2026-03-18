06294f120fb9b355f7b57a6597de3757ca6df9d9
Maria Pol <maria.pujolpol@gmail.com>
2026-03-18 18:15:28 +0100

    Lab done

--- src/iteration2.ts
+++ src/iteration2.ts
@@ -2,4 +2,28 @@
 // Iteration 2 | Functions
 //
 
+function calcMultiplication(a: number, b: number): number { 
+    return a * b;
+}
+/*const result1= calcMultiplication(2,3);
+console.log(result1);
+const result2= calcMultiplication(4,5);
+console.log(result2);*/
 
+function isEven(n: number): boolean {
+    return n % 2 === 0;
+}
+
+/*const result1 = isEven(4);
+console.log(result1);
+
+const result2 = isEven(7);
+console.log(result2);*/
+
+function calcArrayAverage(numbersArr: number[]): number | string {
+    if (numbersArr.length === 0) {
+        return 'Please provide an array with at least one element';
+    }
+    const sum = numbersArr.reduce((acc, num) => acc + num, 0);
+    return sum / numbersArr.length;
+}
--- src/iteration4.ts
+++ src/iteration4.ts
@@ -3,3 +3,35 @@
 //
 
 
+type Book = {
+    title: string;
+    year: number;
+    genres: string[];
+    rating?: number;
+    author: string;
+    numberOfPages?: number;
+}
+
+type Movie = {
+    title: string;
+    year: number;
+    genres: string[];
+    rating?: number;
+    director: string;
+    durationInMinutes?: number;
+}
+
+type MusicAlbum = {
+    title: string;
+    year: number;
+    genres: string[];
+    rating?: number;
+    artist: string;
+    numberOfTracks?: number;
+    durationInMinutes?: number;
+    albumType: 'Studio Album' | 'Live Album' | 'Soundtrack' | 'Other';
+};
+function getYearsSinceRelease(mediaItem: Book | Movie | MusicAlbum): number {
+    const currentYear = new Date().getFullYear();
+    return currentYear - mediaItem.year;
+}
